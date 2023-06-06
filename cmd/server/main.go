88365774d516b309e43b28561d2737483b2117ae
Javier Cisneros <javing77@gmail.com>
2023-06-06 11:19:13 -0500

    Connection to database..

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	db "github.com/javing77/go-rest-postgress/internal/db"
+)
 
 // Run - is going to be responsible for
 // the instantiation and startup of our
 // go application
 func Run() error {
 	fmt.Println("Starting up out application")
+
+	db, err := db.NewDataBase()
+	if err != nil {
+		fmt.Println("failed to connect to database")
+		return err
+	}
+
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/javing77/go-rest-postgress
 
 go 1.20
+
+require (
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/lib/pq v1.10.9
+)
